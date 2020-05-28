21e9db8b68a146b1c735e9210d2848f5c25ca6fe
Muhammad Kashif Zahid butt <muhabutt@gmail.com>
2020-05-28 17:05:30 +0300

    'gitignore'

--- src/jsonFiles/Constants.js
+++
@@ -1,9 +0,0 @@
-const Constants = {
-  images: {
-    logo: require('../../assets/images/logo.png'),
-  },
-  apiKey: 'd3eeefecb83747f2bdb3f12ba259520d',
-  url: 'http://newsapi.org/v2/',
-};
-
-export default Constants;
